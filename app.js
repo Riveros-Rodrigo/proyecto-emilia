688a939da8378d38633db5985633132998a5211a
Rodrigo Riveros <riverosdev@gmail.com>
2023-06-15 12:28:24 -0300

    agrego imagenes

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ app.use(express.static('public')) //ruteo los recursos estaticos
 
 /* RUTAS */
 app.get('/',(req,res) =>res.sendFile(path.join(__dirname,'views','home.html')))
-app.get('/nosotros',(req,res) =>res.sendFile(path.join(__dirname,'views','about.html')))
-app.get('/contacto',(req,res) =>res.sendFile(path.join(__dirname,'views','contact.html')))
-app.get('/musica',(req,res) =>res.sendFile(path.join(__dirname,'views','music.html')))
-app.get('/babage',(req,res) =>res.sendFile(path.join(__dirname,'views','babage.html')))
+app.get('/about',(req,res) =>res.sendFile(path.join(__dirname,'views','about.html')))
+app.get('/contact',(req,res) =>res.sendFile(path.join(__dirname,'views','contact.html')))
+app.get('/music',(req,res) =>res.sendFile(path.join(__dirname,'views','music.html')))
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
 
