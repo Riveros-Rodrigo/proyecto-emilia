938ffa5e700c8287503ba74d82876bb7c1e729df
Rodrigo Riveros <riverosdev@gmail.com>
2023-06-15 16:04:02 -0300

    agrego contenido a home y music

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/',(req,res) =>res.sendFile(path.join(__dirname,'views','home.html')))
 app.get('/about',(req,res) =>res.sendFile(path.join(__dirname,'views','about.html')))
 app.get('/contact',(req,res) =>res.sendFile(path.join(__dirname,'views','contact.html')))
 app.get('/music',(req,res) =>res.sendFile(path.join(__dirname,'views','music.html')))
+app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html'))) //El * es para agarrar todas las rutas si hay una que no es las que tengo me sale el error 404
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
 
